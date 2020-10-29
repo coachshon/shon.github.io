4c135042f109c68815b4dda1435407dfd6efcb39
Ariel Andrade <lima.ariel97@gmail.com>
2020-10-28 21:22:14 -0300

    testing console

--- js/script.js
+++ js/script.js
@@ -110,10 +110,6 @@ function typewriter() {
 document.addEventListener("DOMContentLoaded", function() {
 	document.body.style.overflow = 'hidden';
 	typewriter();
-	console = document.getElementById("console");
-	typedtext = document.getElementById("typedtext");
-	console.style.width = (0.7 * window.screen.width);
-	typedtext.style.width = console.style.width;
 });
 
 function showcontent() {
