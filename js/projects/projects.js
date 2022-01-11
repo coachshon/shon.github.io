a65f4144e3005fd71f74a89519695ae8f4084336
Ariel Andrade <lima.ariel97@gmail.com>
2022-01-11 00:02:24 -0300

    list of Project objects filtered created

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -9,24 +9,44 @@ class Project {
 }
 
 var repos, filter; 
+var reposFiltered = [];
 
 function getProjects() {
-    getGitHubReposJson();
-    getFilterJson()
+    getGitHubReposJson(function(reposJson) {
+        repos = reposJson;
+        getFilterJson(function(filterJson) {
+            filter = filterJson;
+            filterData();
+        });
+    });  
 }
 
-function getGitHubReposJson() {
+function filterData() {
+    console.log(filterRepos(repos, filter));
+}
+
+function getGitHubReposJson(callback) {
     let requestURL = 'https://api.github.com/users/sudoariel/repos';
     let request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
     request.onload = function() {
-        repos = request.response;
+        callback(request.response);
     }
 }
 
-function getFilterJson() {
+function getRepoLanguagesJson(requestURL, callback) {
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        callback(request.response);
+    }
+}
+
+function getFilterJson(callback) {
     let requestURL = 'js/projects/filter.json';
     let request = new XMLHttpRequest();
     request.overrideMimeType('application/json');
@@ -34,6 +54,26 @@ function getFilterJson() {
     request.responseType = 'json';
     request.send();
     request.onload = function() {
-        filter = request.response;
+        callback(request.response);
     }
+}
+
+function filterRepos(reposJson, filterJson) {
+    let newRepos = reposJson.filter(function (repo) {
+        return filterJson.find(repoFilter => repoFilter.repo === repo.name) !== undefined;
+    });
+
+    for(let i = 0; i < newRepos.length; i++) {
+        getRepoLanguagesJson(newRepos[i].languages_url, function(languagesJson) {
+            filterRepoData = filterJson.find(repoFilter => repoFilter.repo == newRepos[i].name)
+            reposFiltered.push(new Project(
+                filterRepoData.title,
+                filterRepoData.repo,
+                newRepos[i].description,
+                newRepos[i].html_url,
+                Object.keys(languagesJson)
+            ));      
+        });
+    }
+    return reposFiltered;
 }
